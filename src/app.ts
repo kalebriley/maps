44a6ffe13d13cdd0efd44f4c738b30ddbb7307c7
Kaleb Riley <kaleb.riley91@gmail.com>
2022-05-08 23:19:15 -0600

    gather user input

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log('working')
+const form = document.querySelector('form')! as HTMLFormElement
+const addressInput = document.getElementById('address')! as HTMLInputElement
+
+
+function searchAddressHandler(e: Event): void {
+    e.preventDefault()
+    const enteredAddress = addressInput.value
+
+    //use google api
+}
